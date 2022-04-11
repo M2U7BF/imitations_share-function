06ba81720d9df7bf24fcc6e8422f0dcd3682069c
M2U7BF <yhvh4321@gmail.com>
2022-04-11 13:43:07 +0900

    modified 2022/04/11

--- users/admin.py
+++ users/admin.py
@@ -6,16 +6,21 @@ from .models import User
 
 # django/contrib/auth/forms.py における UserChangeForm の記述の書き換え
 class MyUserChangeForm(UserChangeForm):
+    # field_classes = {"username": UsernameField} → 削除
+    # classオブジェクト を生成するのが Metaで作成するclass(設計書)(https://stackoverflow.com/questions/100003/what-are-metaclasses-in-python)
+    # username に関する設定を変更したいのだろうか?
     class Meta:
         model = User
         fields = '__all__'
 
 class MyUserCreationForm(UserCreationForm):
+    # field_classes = {"username": UsernameField} → 削除
     class Meta:
         model = User
         fields = ('email','username')
 
 class MyUserAdmin(UserAdmin):
+    # 変更
     fieldsets = (
         (None, {'fields': ('email', 'password', 'username')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
@@ -25,14 +30,22 @@ class MyUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
+            # usernameの表記の削除
             'fields': ('email', 'password1', 'password2'),
         }),
     )
+    # 独自のものに変更
     form = MyUserChangeForm
+    # 独自のものに変更
     add_form = MyUserCreationForm
+    # first_name last_name の削除
     list_display = ('email', 'username', 'is_staff')
+    # 原文
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    # first_name last_name の削除
     search_fields = ('email', 'username')
+    # username → email の変更
     ordering = ('email',)
 
+# 管理画面への表示
 admin.site.register(User, MyUserAdmin)
