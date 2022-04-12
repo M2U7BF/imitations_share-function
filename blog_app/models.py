87d2b4eb7b8d83d9eaf724f917a8d6e63583c164
M2U7BF <yhvh4321@gmail.com>
2022-04-12 16:08:44 +0900

    modified

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,4 @@
 from email.policy import default
-from tkinter import CASCADE
 from django.db import models
 from django.utils import timezone
 # (https://office54.net/python/django/model-app-import)
