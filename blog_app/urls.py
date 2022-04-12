aa6786cd138a2d2af77632ec33657f4e587e69c4
M2U7BF <yhvh4321@gmail.com>
2022-04-12 15:34:35 +0900

    before heroku upload

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('home_page/<userid>', views.HomePageView.as_view(), name='home_page'),
+    path('home_page/mypage/<userid>', views.MyPageView.as_view(), name='my_page'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -5,6 +5,45 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.utils.translation import gettext as _
 
+class MyPageView(DetailView):
+    template_name = 'my_home.html'
+    model = ArticleModel
+
+    def get_context_data(self, **kwargs):
+        ctxt = super().get_context_data()
+        ctxt["object_list"] = ArticleModel.objects.all()
+        ownerPk = self.kwargs['userid']
+        ctxt['page_owner'] = User.objects.get(pk=ownerPk)
+        return ctxt
+    
+    # オーバーライド
+    # get_object()は何をしているのか? → 
+    # urlに値する1つのデータを取得
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        
+        # request に 合致するデータを取得
+        # return get_object_or_404(User, pk=self.request.session['user_id'])
+        # pk = self.kwargs.get(self.request)
+        # 原文
+        pk = self.kwargs['userid']
+        # pk = self.kwargs.get(self.pk_url_kwarg)
+        # pk
+        if pk is not None:
+            # この時点で単数にする(?)
+            queryset = queryset.filter(posted_by=pk)
+        
+        try:
+            # Get the single item from the filtered queryset
+            objs = queryset.filter()
+        except queryset.model.DoesNotExist:
+            raise Http404(
+                _("No %(verbose_name)s found matching the query")
+                % {"verbose_name": queryset.model._meta.verbose_name}
+            )
+        return objs
+
 # Create your views here.
 class HomePageView(DetailView):
     template_name = '_main.html'
@@ -13,28 +52,37 @@ class HomePageView(DetailView):
     def get_context_data(self, **kwargs):
         ctxt = super().get_context_data()
         ctxt["object_list"] = ArticleModel.objects.all()
+        ownerPk = self.kwargs['userid']
+        ctxt['page_owner'] = User.objects.get(pk=ownerPk)
         return ctxt
     
     # オーバーライド
+    # get_object()は何をしているのか? → 
+    # urlに値する1つのデータを取得
     def get_object(self, queryset=None):
         if queryset is None:
             queryset = self.get_queryset()
         
         # request に 合致するデータを取得
         # return get_object_or_404(User, pk=self.request.session['user_id'])
-        pk = self.kwargs.get(self.request)
+        # pk = self.kwargs.get(self.request)
+        # 原文
+        pk = self.kwargs['userid']
+        # pk = self.kwargs.get(self.pk_url_kwarg)
+        # pk
         if pk is not None:
-            queryset = queryset.filter(pk=pk)
+            # この時点で単数にする(?)
+            queryset = queryset.filter(posted_by=pk)
         
         try:
             # Get the single item from the filtered queryset
-            obj = queryset.get()
+            objs = queryset.filter()
         except queryset.model.DoesNotExist:
             raise Http404(
                 _("No %(verbose_name)s found matching the query")
                 % {"verbose_name": queryset.model._meta.verbose_name}
             )
-        return obj
+        return objs
 
 class index_view(TemplateView):
     template_name="index.html"
